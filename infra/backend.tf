90d2755db5de9ce9f3e84719ca374338d6d71222
fawasm303-stack <fawasmakkar303@gmail.com>
2026-02-09 15:08:31 +0400

    Update backend.tf

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "terraform-statefile-fp"
-    key          = "rails-api/terraform.tfstate"
+    key          = "fm-rails-api/terraform.tfstate"
     region       = "me-central-1"
     encrypt      = true
     use_lockfile = true
